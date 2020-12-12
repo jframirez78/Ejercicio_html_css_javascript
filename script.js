4b70956fabb15ffeefad427a2eb406379c161bd1
jframirez78 <jframirez78@misena.edu.co>
2020-12-11 22:02:48 -0500

    combio el nombre de la funcion en javascript y aun no funciona

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-function validacion(){
-    console.log("validacion")
+function credencial(){
+    console.log("credencial");
+    
     var email = document.getElementById("email").value;
     var password = document.getElementById("password").value;
      if (email == "jframirez78@misena.edu.co" & password == "1234"){
         location.href="www.google.com";
 }
-     else {document.getElementById ("validacion").innerHTML="No coincide"
-}
+     else {document.getElementById("credencial").innerHTML="No coincide"
+    }
 
 }
    
